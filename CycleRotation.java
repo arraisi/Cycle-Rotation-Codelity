4c7ef5000b38ac2ac00dd571390878413c5248bc
Abdul Rahman Arraisi <itsarraisi@gmail.com>
2018-08-28 07:00:21 +0700

    create new program

---
+++ CycleRotation.java
@@ -0,0 +1,13 @@
+public class CycleRotation {
+    public static void main(String[] args) {
+        CycleRotation cycleRotation = new CycleRotation();
+        int[] shop = {1,2,3,4};
+        int i = 2;
+        cycleRotation.solution(shop, i);
+    }
+
+    public static int[] solution(int[] A, int K) {
+
+        return A;
+    }
+}
