4a20961790b8c4eea3e2abda2e63e31a889eecda
Abdul Rahman Arraisi <38418401+arraisi@users.noreply.github.com>
2018-09-07 11:07:16 +0700

    Update CycleRotation.java

--- CycleRotation.java
+++ CycleRotation.java
@@ -11,3 +11,30 @@ public class CycleRotation {
         return A;
     }
 }
+
+// UPDATE SOLUTION 7-September-2018
+    // you can also use imports, for example:
+// import java.util.*;
+
+// you can write to stdout for debugging purposes, e.g.
+// System.out.println("this is a debug message");
+
+// class Solution {
+//     public int[] solution(int[] A, int K) {
+//         // write your code in Java SE 8
+//         int[] NewArray = new int[A.length];
+//         int i = 0;
+//         if (K%2 == 0) {
+//             return A;
+//         }
+//         for (int temp : A){
+//             if ((i+K) < A.length ) {
+//                 NewArray[i+K] = temp;
+//             } else {
+//                 NewArray[i+1-K] = temp;    
+//             }
+//             i++;
+//         }
+//         return NewArray;
+//     }
+// }
